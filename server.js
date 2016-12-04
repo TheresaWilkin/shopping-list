9f540775442a58f60422786641870c06e0b5ac01
Theresa Wilkin <theresawilkin@yahoo.com>
2016-12-04 15:57:57 -0500

    shopping list

---
+++ server.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+var Storage = {
+  add: function(name) {
+    var item = {name: name, id: this.setId};
+    this.items.push(item);
+    this.setId += 1;
+    return item;
+  } ,
+  findIndex: function(id) {
+  	for (var i = 0; i < this.items.length; i++) {
+  		if (this.items[i].id === parseInt(id)) {
+  			var item = this.items[i];
+  			return i;
+  		}
+  	}
+  	return undefined;
+  },
+  delete: function(id) {
+  	var index = this.findIndex(id);
+  	var item = this.items.splice(index, 1);
+  	return item;
+  },
+  update: function(id, item) {
+  	var index = this.findIndex(id);
+  	this.items[index] = item;
+  	return item;
+  }
+};
+
+var createStorage = function() {
+  var storage = Object.create(Storage);
+  storage.items = [];
+  storage.setId = 1;
+  return storage;
+}
+
+var storage = createStorage();
+
+storage.add('Broad beans');
+storage.add('Tomatoes');
+storage.add('Peppers');
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/items', function(request, response) {
+    response.status(200).json(storage.items);
+});
+
+app.post('/items', jsonParser, function(request, response) {
+	if (!('name' in request.body)) {
+        return response.sendStatus(400);
+    }
+	var item = storage.add(request.body.name);
+	response.status(201).json(item);
+})
+
+app.delete('/items/:id', jsonParser, function(request, response) {
+	if (storage.findIndex(request.params.id) === undefined) {
+		return response.status(404).json({message: "Item not found"});
+	}
+	var item = storage.delete(request.params.id);
+	response.status(200).json(item);
+})
+
+app.put('/items/:id', jsonParser, function(request, response) {
+	if (!request.body.name || !request.body.id) {
+		return response.status(400).json({message:"Item not found"});
+	}
+	if (request.body.id != request.params.id) {
+		return response.status(400).json({message: "ID doesn't match"});
+	}
+	if (storage.findIndex(request.params.id) === undefined) {
+		var item = storage.add(request.body.name);
+		response.status(201).json(item);
+	}
+	var item = storage.update(request.params.id, request.body);
+	response.status(200).json(item);
+})
+
+app.listen(process.env.PORT || 8080, process.env.IP);
+
